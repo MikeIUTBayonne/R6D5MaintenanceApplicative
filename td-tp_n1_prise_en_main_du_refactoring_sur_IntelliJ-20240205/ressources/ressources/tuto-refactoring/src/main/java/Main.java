d54f67fd81ecaea01898adb40a505ef66af9ea4e
mmonimaud <mmonimaud@iutbayonne.univ-pau.fr>
2024-02-06 14:37:06 +0100

    Extraction de la méthode 'isDateBetween'

--- td-tp_n1_prise_en_main_du_refactoring_sur_IntelliJ-20240205/ressources/ressources/tuto-refactoring/src/main/java/Main.java
+++ td-tp_n1_prise_en_main_du_refactoring_sur_IntelliJ-20240205/ressources/ressources/tuto-refactoring/src/main/java/Main.java
@@ -11,8 +11,13 @@ public class Main {
     }
     public static boolean isNowBetween(LocalDate startingDate, LocalDate endingDate) {
         LocalDate now = LocalDate.now();
-        return now.isAfter(startingDate) && now.isBefore(endingDate);
+        return isDateBetween(now, startingDate, endingDate);
     }
+
+    private static boolean isDateBetween(LocalDate date, LocalDate startingDate, LocalDate endingDate) {
+        return date.isAfter(startingDate) && date.isBefore(endingDate);
+    }
+
     public static void main(String[] args) {
         new Main().add(1, 2);
         System.out.printf("Hello World");
