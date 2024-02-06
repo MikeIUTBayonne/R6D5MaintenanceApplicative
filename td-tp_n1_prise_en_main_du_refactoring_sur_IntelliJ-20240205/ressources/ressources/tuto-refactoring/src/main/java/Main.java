7f6f84233d7e1bb9704579ac7ad0bfeac3421cd5
mmonimaud <mmonimaud@iutbayonne.univ-pau.fr>
2024-02-06 12:20:22 +0100

    Ajout de la Javadoc + Changement de nom de la variable 'a' en 'firstNumber'

--- td-tp_n1_prise_en_main_du_refactoring_sur_IntelliJ-20240205/ressources/ressources/tuto-refactoring/src/main/java/Main.java
+++ td-tp_n1_prise_en_main_du_refactoring_sur_IntelliJ-20240205/ressources/ressources/tuto-refactoring/src/main/java/Main.java
@@ -1,6 +1,11 @@
 public class Main {
-    public int add(int a, int b) {
-        return a + b;
+    /**
+     * Adds a and b
+     * @param firstNumber the first number
+     * @param b the second number
+     */
+    public int add(int firstNumber, int b) {
+        return firstNumber + b;
     }
     public static void main(String[] args) {
         new Main().add(1, 2);
