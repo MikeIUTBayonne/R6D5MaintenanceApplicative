319cb576be1d4b309f232d56e8229bcccb3d88ff
mmonimaud <mmonimaud@iutbayonne.univ-pau.fr>
2024-02-06 11:52:51 +0100

    Ajout de la méthode 'someAdditionMethod'

--- td-tp_n1_prise_en_main_du_refactoring_sur_IntelliJ-20240205/ressources/ressources/tuto-refactoring/src/main/java/Main.java
+++ td-tp_n1_prise_en_main_du_refactoring_sur_IntelliJ-20240205/ressources/ressources/tuto-refactoring/src/main/java/Main.java
@@ -1,6 +1,9 @@
 public class Main {
-
+    public int someAdditionMethod(int a, int b) {
+        return a + b;
+    }
     public static void main(String[] args) {
+        new Main().someAdditionMethod(1, 2);
         System.out.printf("Hello World");
     }
 }
