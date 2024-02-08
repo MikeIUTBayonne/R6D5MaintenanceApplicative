ff72f368848f960bb0d8dd2ec3f6cad213345719
mmonimaud <mmonimaud@iutbayonne.univ-pau.fr>
2024-02-08 10:38:29 +0100

    Extraction de la méthode 'addFrequentRenterPoints'

--- td-tp n°2  exercices de refactoring basés sur katas-20240208/movie-rental-java-master/src/main/java/movierental/Customer.java
+++ td-tp n°2  exercices de refactoring basés sur katas-20240208/movie-rental-java-master/src/main/java/movierental/Customer.java
@@ -28,11 +28,8 @@ public class Customer {
         for (Rental each : _rentals) {
             double thisAmount = determineAmountForEachLine(each);
 
-            // add frequent renter points
-            frequentRenterPoints++;
-            // add bonus for a two-day new release rental
-            if ((each.getMovie().getPriceCode() == Movie.NEW_RELEASE) && each.getDaysRented() > 1)
-                frequentRenterPoints++;
+            int rentalFrequentRenterPoints = addFrequentRenterPoints(each);
+            frequentRenterPoints += rentalFrequentRenterPoints;
 
             // show figures for this rental
             result += "\t" + each.getMovie().getTitle() + "\t" + String.valueOf(thisAmount) + "\n";
@@ -46,10 +43,18 @@ public class Customer {
         return result;
     }
 
+    private static int addFrequentRenterPoints(Rental each) {
+        // add frequent renter points
+        int rentalFrequentRenterPoints = 1;
+        // add bonus for a two-day new release rental
+        if ((each.getMovie().getPriceCode() == Movie.NEW_RELEASE) && each.getDaysRented() > 1)
+            rentalFrequentRenterPoints += 1;
+        return rentalFrequentRenterPoints;
+    }
+
     private static double determineAmountForEachLine(Rental each) {
         double thisAmount = 0;
 
-        //determine amounts for each line
         switch (each.getMovie().getPriceCode()) {
             case Movie.REGULAR:
                 thisAmount += 2;
