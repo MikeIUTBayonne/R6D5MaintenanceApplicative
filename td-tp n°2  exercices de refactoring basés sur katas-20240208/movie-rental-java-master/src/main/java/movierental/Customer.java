ef4b81dfad54340ae7a46b03bacb8cb8e0395d25
mmonimaud <mmonimaud@iutbayonne.univ-pau.fr>
2024-02-08 11:00:51 +0100

    Renommage des variables pour plus de cohérence

--- td-tp n°2  exercices de refactoring basés sur katas-20240208/movie-rental-java-master/src/main/java/movierental/Customer.java
+++ td-tp n°2  exercices de refactoring basés sur katas-20240208/movie-rental-java-master/src/main/java/movierental/Customer.java
@@ -5,19 +5,19 @@ import java.util.List;
 
 public class Customer {
 
-    private String _name;
-    private List<Rental> _rentals = new ArrayList<Rental>();
+    private String name;
+    private List<Rental> rentals = new ArrayList<Rental>();
 
     public Customer(String name) {
-        _name = name;
+        this.name = name;
     }
 
     public void addRental(Rental arg) {
-        _rentals.add(arg);
+        rentals.add(arg);
     }
 
     public String getName() {
-        return _name;
+        return name;
     }
 
     public String statement() {
@@ -25,13 +25,13 @@ public class Customer {
         int frequentRenterPoints = 0;
         String result = addHeaderLine();
 
-        for (Rental each : _rentals) {
-            double thisAmount = determineAmountForEachLine(each);
+        for (Rental rental : rentals) {
+            double thisAmount = determineAmountForEachLine(rental);
 
-            int rentalFrequentRenterPoints = addFrequentRenterPoints(each);
+            int rentalFrequentRenterPoints = addFrequentRenterPoints(rental);
             frequentRenterPoints += rentalFrequentRenterPoints;
             
-            result += showFiguresForThisRental(each, thisAmount);
+            result += showFiguresForThisRental(rental, thisAmount);
             totalAmount += thisAmount;
         }
 
@@ -45,38 +45,38 @@ public class Customer {
     }
 
     private static String addFooterLines(double totalAmount, int frequentRenterPoints) {
-        return "Amount owed is " + String.valueOf(totalAmount) + "\n" + "You earned " + String.valueOf(frequentRenterPoints) + " frequent renter points";
+        return "Amount owed is " + totalAmount + "\n" + "You earned " + frequentRenterPoints + " frequent renter points";
     }
 
     private static String showFiguresForThisRental(Rental each, double thisAmount) {
-        return "\t" + each.getMovie().getTitle() + "\t" + String.valueOf(thisAmount) + "\n";
+        return "\t" + each.getMovie().getTitle() + "\t" + thisAmount + "\n";
     }
 
-    private static int addFrequentRenterPoints(Rental each) {
+    private static int addFrequentRenterPoints(Rental rental) {
         // add frequent renter points
         int rentalFrequentRenterPoints = 1;
         // add bonus for a two-day new release rental
-        if ((each.getMovie().getPriceCode() == Movie.NEW_RELEASE) && each.getDaysRented() > 1)
+        if ((rental.getMovie().getPriceCode() == Movie.NEW_RELEASE) && rental.getDaysRented() > 1)
             rentalFrequentRenterPoints += 1;
         return rentalFrequentRenterPoints;
     }
 
-    private static double determineAmountForEachLine(Rental each) {
+    private static double determineAmountForEachLine(Rental rental) {
         double thisAmount = 0;
 
-        switch (each.getMovie().getPriceCode()) {
+        switch (rental.getMovie().getPriceCode()) {
             case Movie.REGULAR:
                 thisAmount += 2;
-                if (each.getDaysRented() > 2)
-                    thisAmount += (each.getDaysRented() - 2) * 1.5;
+                if (rental.getDaysRented() > 2)
+                    thisAmount += (rental.getDaysRented() - 2) * 1.5;
                 break;
             case Movie.NEW_RELEASE:
-                thisAmount += each.getDaysRented() * 3;
+                thisAmount += rental.getDaysRented() * 3;
                 break;
             case Movie.CHILDRENS:
                 thisAmount += 1.5;
-                if (each.getDaysRented() > 3)
-                    thisAmount += (each.getDaysRented() - 3) * 1.5;
+                if (rental.getDaysRented() > 3)
+                    thisAmount += (rental.getDaysRented() - 3) * 1.5;
                 break;
         }
         return thisAmount;
