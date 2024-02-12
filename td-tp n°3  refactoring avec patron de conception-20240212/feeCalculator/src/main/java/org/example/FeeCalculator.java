3c5480729d2598648cde8c45a84e85c5ce80a1fc
mmonimaud <mmonimaud@iutbayonne.univ-pau.fr>
2024-02-12 18:06:50 +0100

    Extraction des méthodes 'calculateAdultFee' et 'calculateChildrenFee'

--- td-tp n°3  refactoring avec patron de conception-20240212/feeCalculator/src/main/java/org/example/FeeCalculator.java
+++ td-tp n°3  refactoring avec patron de conception-20240212/feeCalculator/src/main/java/org/example/FeeCalculator.java
@@ -7,19 +7,30 @@ public class FeeCalculator {
     public static double calculateFee(Visitor visitor, TicketType ticketType) {
         double fee = 0;
 
-        // calculate price for adults
-        if ((visitor.getAge() > 14) && (TicketType.HALF_DAY == ticketType)) {
-            fee = ADULT_PRICE_BASE * 0.6;
-        } else if ((visitor.getAge() > 14) && (TicketType.FULL_DAY == ticketType)) {
-            fee = ADULT_PRICE_BASE * 1.2;
+        if (visitor.getAge() > 14){
+            fee = calculateAdultFee(visitor, ticketType);
         }
-
-        // calculate price for children
-        if ((visitor.getAge() <= 14) && (TicketType.HALF_DAY == ticketType)) {
-            fee = ADULT_PRICE_BASE * 0.2;
-        } else if ((visitor.getAge() <= 14) && (TicketType.FULL_DAY == ticketType)) {
-            fee = ADULT_PRICE_BASE * 0.5;
+        if (visitor.getAge() <= 14){
+            fee = calculateChildrenFee(visitor, ticketType);
         }
         return fee;
+    }
+
+    private static double calculateChildrenFee(Visitor visitor, TicketType ticketType) {
+        if (TicketType.HALF_DAY == ticketType) {
+            return ADULT_PRICE_BASE * 0.2;
+        } else if (TicketType.FULL_DAY == ticketType) {
+            return ADULT_PRICE_BASE * 0.5;
+        }
+        return 0;
+    }
+
+    private static double calculateAdultFee(Visitor visitor, TicketType ticketType) {
+        if (TicketType.HALF_DAY == ticketType) {
+            return CHILD_PRICE_BASE * 0.6;
+        } else if (TicketType.FULL_DAY == ticketType) {
+            return CHILD_PRICE_BASE * 1.2;
+        }
+        return 0;
     }
 }
